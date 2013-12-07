eae9a779c0ec55b9c18b22ec44b56465bc3bdd06
David Nyffenegger <david.nyffenegger@namics.com>
2013-12-07 21:07:38 +0100

    adds order overview

--- static/js/orders.js
+++ static/js/orders.js
@@ -5,9 +5,52 @@
 
 function OrdersController($scope, $http, messageService) {
 
+    var calcTotal = function() {
+        $scope.total = {
+            cash: {},
+            voucher: {
+                amount: 0,
+                currency: '',
+                count: 0
+            },
+            volume: {},
+            kitchenOrderCount: 0,
+            orderCount: 0,
+            articleCounts: {}
+        };
+
+        angular.forEach($scope.orders, function(order){
+            $scope.total.orderCount++;
+            if (order.kitchen){
+                $scope.total.kitchenOrderCount++;
+            }
+            if (order.voucher) {
+                $scope.total.voucher.amount += order.total;
+                $scope.total.voucher.currency = order.currency;
+                $scope.total.voucher.count++;
+            } else {
+                var cash = $scope.total.cash[order.currency] || 0;
+                cash += order.total;
+                $scope.total.cash[order.currency] = cash;
+            }
+
+            angular.forEach(order.articles, function(article){
+                var articleCount = $scope.total.articleCounts[article.name] || 0;
+                articleCount += article.ordered;
+                $scope.total.articleCounts[article.name] = articleCount;
+                angular.forEach(article.price, function(price, currency){
+                    var volume = $scope.total.volume[currency] || 0;
+                    volume += price * article.ordered;
+                    $scope.total.volume[currency] = volume;
+                });
+            });
+        })
+    }
+
     $http.get('/orders')
         .success(function(orders){
            $scope.orders = orders;
+            calcTotal();
         });
 
     $scope.select = function(order) {
@@ -30,7 +73,8 @@ function OrdersController($scope, $http, messageService) {
             $scope.selectedOrder = undefined;
             $scope.removeEnabled = false;
         })
-    }
+    };
+
 
 }
 
